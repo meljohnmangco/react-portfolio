7fceb5735840c3c9052e233c35de77dd05aad72f
landeroov <meljohn7.mm@gmail.com>
2022-12-28 13:48:48 +0800

    replace icon

--- src/scenes/Services.jsx
+++ src/scenes/Services.jsx
@@ -26,7 +26,7 @@ const Services = ({setSelectedPage}) => {
                             <div class="iconbox rounded-3">
                             <i class="lab la-react"></i>
                             </div>
-                            <h5 class="mt-4 mb-2">React.js</h5>
+                            <h5 class="mt-4 mb-2">React.js Web Development</h5>
                             <p class="cutoff-lines">
                             I specialize in React.js Development, providing high-quality services for web application development using the React.js framework. I can help bring your web project to life with intuitive and reactive user interfaces that deliver a seamless user experience. Whether you need a simple single-page application or a complex, data-intensive platform, my React.js Development services can help make your project a success. Contact me today to learn more about how my expertise in React.js can benefit your business.
                             </p>
@@ -46,7 +46,7 @@ const Services = ({setSelectedPage}) => {
                     >
                         <div className="service p-4 bg-base zoom zoom-line">
                             <div className="iconbox rounded-3">
-                            <i className="las la-store"></i>
+                            <i class="las la-tools"></i>
                             </div>
                             <h5 className="mt-4 mb-2">Maintenance and Support</h5>
                             <p className="cutoff-lines">
@@ -70,7 +70,7 @@ const Services = ({setSelectedPage}) => {
                             <div className="iconbox rounded-3">
                             <i className="las la-pencil-alt"></i>
                             </div>
-                            <h5 className="mt-4 mb-2">Responsive Design</h5>
+                            <h5 className="mt-4 mb-2">Responsive Design Development</h5>
                             <p className="cutoff-lines">
                             I specialize in Responsive Web Design, providing tailored solutions that ensure your website looks great and functions seamlessly on any device. I have the skills and expertise to create responsive websites that are optimized for mobile, tablet, and desktop environments. With my responsive design approach, I can help your website adapt to different screen sizes and resolutions, delivering a consistent and user-friendly experience for your visitors. Contact me today to learn more about how our responsive web design services can benefit your business.
                             </p>
